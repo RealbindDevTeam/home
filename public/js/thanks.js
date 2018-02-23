d9d820abd606cf3017cd426bf07b3c2728f477cd
Fernando Arévalo <fernando.arevalo9311@gmail.com>
2018-02-23 16:56:17 -0500

    Se cambian las imagenes del logo iurest por las de come y gana

--- public/js/thanks.js
+++ public/js/thanks.js
@@ -7,6 +7,6 @@ function thanksRedirect() {
     if (submitted) {
         var loadingDiv = document.getElementById('spinner');
         loadingDiv.style.display="block";
-        setTimeout(function(){ window.location = 'http://localhost:3001/gracias'; }, 3000);
+        setTimeout(function(){ window.location = 'http://www.iurest.com/gracias'; }, 3000);
     }
 }
